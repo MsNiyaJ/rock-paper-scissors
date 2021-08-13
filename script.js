72f9e59f8673fceda9ecba3d5db1a4fe932a3b2b
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-08-12 23:49:52 -0400

    moved all javascript to script.js

---
+++ script.js
@@ -0,0 +1,88 @@
+let winner; //Winner of the round
+    
+//The computer randomly selects 'rock', 'paper', or 'scissors'
+function computerPlay(){
+    const computerChoices = ['Rock', 'Paper', 'Scissors'];
+    let i = Math.floor(Math.random() * 3);
+    let computerChoice = computerChoices[i];
+    return computerChoice;
+}
+
+// Determines who is the winner of the round and returns a message
+function playRound(playerSelection, computerSelection){
+    console.log("You chose " + playerSelection);
+    console.log("The computer chose " + computerSelection);
+    
+    let rock = paper = scissors = false;
+    
+    // Set the values true if player or computer chose rock, paper, or scissors
+    if(playerSelection === 'Rock' || computerSelection === 'Rock') 
+        rock = true; 
+    if(playerSelection === 'Paper' || computerSelection === 'Paper') 
+        paper = true;
+    if(playerSelection == 'Scissors' || computerSelection === 'Scissors') 
+        scissors = true;
+    
+    //paper beats rock
+    if(paper && rock){
+        (playerSelection === 'Paper') ? winner = 'You' : winner = 'Computer';
+        return (`${winner} Won! Paper beats Rock!`);
+    }
+    //scissors beats paper
+    else if(scissors && paper){
+        (playerSelection === 'Scissors') ? winner = 'You' : winner = 'Computer';
+        return (`${winner} Won! Scissors beats Paper!`);
+    }
+    //rock beats scissors
+    else if(rock && scissors){
+        (playerSelection === 'Rock') ? winner = 'You' :  winner = 'Computer';
+        return (`${winner} Won! Rock beats Scissors!`);
+    }
+    else if(playerSelection === computerSelection)
+        return (`It\'s a tie! You both chose ${playerSelection}!`);   
+    else
+        return ('Something went wrong!');
+}
+
+// Makes it so only the first letter of a word is capitalized.
+function changeCase(playerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+}
+
+//Starts the game, there are five rounds in a game
+function game(){
+    //Counts the number of wins each player has
+    let playerWins = 0;
+    let computerWins = 0;
+    let playerSelection;
+
+    for(let i = 1; i < 6; i++){
+        //Prompts the user until they enter a valid answer
+        let invalid = true;
+        while(invalid){
+            playerSelection = prompt("Round " + i + ":\nRock , Paper, or Scissors?");
+            playerSelection = changeCase(playerSelection);
+            if(playerSelection === 'Rock' || 
+                playerSelection === 'Paper' ||
+                playerSelection === 'Scissors'){
+                invalid = false;
+            }
+        }
+        
+        // Starts the round then displays who the winner is
+        let msg = playRound(playerSelection, computerPlay());
+        console.log(msg);
+        console.log(''); 
+
+        if(winner === "You") playerWins++;
+        if(winner === "Computer") computerWins++;
+    }
+
+    // Displays who is the winner of the game and how many rounds they won
+    if(playerWins > computerWins) alert(`YOU WON THE GAME! Score: ${playerWins} / 5 rounds!`);
+    else if(playerWins == computerWins) alert('It was a tie! Good job!')
+    else alert(`YOU LOSE! Score: ${playerWins} / 5 rounds!`);
+}
+
+console.log(game());
